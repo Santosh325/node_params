cb842a59600d986925fd3bc8581a5e8edac0e7fd
santosh325 <santoshthapa.ts12@gmail.com>
2021-09-20 19:03:19 +0545

    passing parameter to endpoint

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const PORT = 2000;
+const friends = [
+    {
+        id: 0,
+        name: 'santosh thap'
+    },
+    {
+        id: 1,
+        name: 'Krishna raja'
+    },
+    {
+        id: 2, 
+        name: "johngffg doe"
+    }, {
+        id: 3,
+        name: 'ljlj austin'
+    },
+    {
+        id: 4,
+        name: 'Hariram austin'
+    },
+    {
+        id: 5,
+        name: 'johntak austin'
+    }
+];
+
+var myLogger 
+
+
+app.get('/friends', (req, res) => {
+    res.json(friends);
+});
+
+app.get('/friends/:friendId', (req,res) => {
+   const friendId = Number(req.params.friendId);
+   const friend = friends[friendId];
+
+   if(friend) {
+       res.status(200).json(friend);
+   } else {
+       res.status(404).json({
+           error: "Friends doesnot exit",
+       })
+   }
+});
+
+app.listen(PORT
+    // console.log(`listening on port ${PORT}`)
+);
